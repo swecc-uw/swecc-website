8148d18cbee736abbce7d9eec8cd64685801d8f3
TophTheTofu <122495765+TophTheTofu@users.noreply.github.com>
2023-04-09 12:07:55 -0700

    Header version 1 is complete? Feeback encouraged

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import SWECClogo from './SWECClogo.jpg';
+
+function Header() {
+  return (
+    <nav class = "full-nav">
+      <div class = "header-logo">
+          <img id = "nav-logo" src = {SWECClogo} alt = "SWECC Logo"/>
+      </div>
+      <div class = "nav-bar">
+        <a href="#" class = "header-pages"> HOME   </a>
+        <a href="#" class = "header-pages"> EVENTS   </a>
+        <a href="#" class = "header-pages"> ABOUT   </a>
+        <a href="#" id = "join-now-btn" class = "header-pages"> JOIN NOW </a>
+      </div>
+    </nav>
+  )
+}
 
 function App() {
   return (
-    <h1> Sophie </h1>
-    <h1> Tacher</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
