59dbe2dc2ef8756162cc8d7ff3abbf598ce84f68
JaydenBalajadia <121709526+JaydenBalajadia@users.noreply.github.com>
2023-04-14 16:05:23 +0000

    Updating About Page w/ Message and Goal

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './About.css';
+const About = () => {
+    return (
+        <about>
+            <div class="about-section">
+                <div class="about-message-section">
+                    <p1>This is the about message</p1>
+                </div>
+                <div class="goal-message-section">
+                    <p1>This is the goal message</p1>
+                </div>
+            </div>
+        </about>
+    );
+}
+
+export default About 
