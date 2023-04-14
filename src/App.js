abc444ad5bb022f982004c08bdd214f34c475451
Nathan <nathan55@uw.edu>
2023-04-13 17:58:56 -0700

    Made the homepage responsive"
    "Made the homepage responsive"

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 function homepage() {
   return (
     <div id="homepage">
+      <style>
+@import url('https://fonts.googleapis.com/css2?family=Assistant&display=swap');
+      </style>
       <div id="clubTitleSection">
         <img id="uwLogo" src="https://s3-alpha-sig.figma.com/img/52b1/c8c0/6479893d9df58e791f33334295c4d07f?Expires=1681689600&Signature=boJ9Vn5IJQC0CG0R94o~iMvDAKoM9QCtD7umhko7wGEe1pBx~5BRD2hN~Nsc~IoeF~IFW9oxUw8CPk~Ch64l1ysp76IGRp13kUdmF8bybi4YeMCnrOcyNSPF5Unmtc0Sa4oo3eAu99FpjShD--X1~YrQ~PNrBd8dLMhcUguXwyZ5MULnu5vDwRoMmDtJ9vA~oVPrJ~q0WIWR8uI4WkslWUt9xhXd52znQ6TCAKtfYKBnVufy~hktLiTLfNC611CW0g9zN2pQJxPtvZ2Y4TDNB5awhC8qQTjfBZARbOdM25fLCR4Jtz5zMEgwj3Sd64CqP6X0C6Gxv6-KQh3FuOKH-w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"/>
         <h1 id="clubName">Software Engineering Career Club</h1>
       </div>
       <div id="whatWeDoSection">
-        <h2>What we do</h2>
+        <h2 id="summaryTitle">What we do</h2>
         <p id="summary">Software Engineering Career Club (also known as SWECC) is a student-led organization at the University of Washington based in Seattle.  Our club's mission is to be a space for people that want to pursue SWE as a career. Within this space, our club hopes to help develop skills and provide resources that can improve students' odds of being successful in their future SWE career. We host events focusing on networking, LeetCode, mentor-mentee pairing, possible career paths, workshopping application material, mock interviews, and other activities that will add value to our members.</p>
       </div>
       <div id="meetingsSection">
@@ -22,10 +25,8 @@ function homepage() {
             Location: Loew Hall Room 215
           </p>
         </div>
-        <div id="map">
-          <img id="mapPic" src="https://s3-alpha-sig.figma.com/img/cf6c/1286/9dac4073f9cb7e44b5cf9b6625693dfe?Expires=1682294400&Signature=ZDjpzhAphkKbqQdKOVbJaduhjOhA-W2sOhQdcwC27S-jpDi7pZzt-8NvW94EkHW6DEOPhRAUUGk-UUhIv~qGrc2Kw4zenHp~hPgK5lGCMOb3m9QhXhSAmO3uubIsCnJ46mBnSNG31SU8r8c8NU~MyGPh9sgjZkZvEUYRh~vfUQGWLYAg25AbjPuPJhpgSE3dpUWYIKV9mBM02KUrlNmyHLMQLYQZ24EpfjOkFp3zT6V49YY~SF~wQ1hDZg49nxuL9l8-XaiQ8I32y3HfAILwDfRKX~3THTaxyQ83t1KXFk-RSnUHz869UMXqPHNRTYaXGNtjtblrbjOmwXVlb4Jxmg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"/>
-          <p id="mapLabel">Location: Loew Hall Room 215</p>
-        </div> 
+        <img id="map" src="https://s3-alpha-sig.figma.com/img/cf6c/1286/9dac4073f9cb7e44b5cf9b6625693dfe?Expires=1682294400&Signature=ZDjpzhAphkKbqQdKOVbJaduhjOhA-W2sOhQdcwC27S-jpDi7pZzt-8NvW94EkHW6DEOPhRAUUGk-UUhIv~qGrc2Kw4zenHp~hPgK5lGCMOb3m9QhXhSAmO3uubIsCnJ46mBnSNG31SU8r8c8NU~MyGPh9sgjZkZvEUYRh~vfUQGWLYAg25AbjPuPJhpgSE3dpUWYIKV9mBM02KUrlNmyHLMQLYQZ24EpfjOkFp3zT6V49YY~SF~wQ1hDZg49nxuL9l8-XaiQ8I32y3HfAILwDfRKX~3THTaxyQ83t1KXFk-RSnUHz869UMXqPHNRTYaXGNtjtblrbjOmwXVlb4Jxmg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"/>
+        <p id="mapLabel">Location: Loew Hall Room 215</p>
       </div>
     </div>
   );
