35a65013cd94c533386312b933f4fe170daaa1ad
JaydenBalajadia <121709526+JaydenBalajadia@users.noreply.github.com>
2023-04-15 22:09:26 +0000

    Aligned text and fixed image background

--- src/About.js
+++ src/About.js
@@ -8,13 +8,13 @@ const About = () => {
             </style>
             <div class="about-section">
                 <div class="about-message-section">
-                    <h1 class="about-title">About</h1>
-                    <p class="about-text">Learn the ways that SWECC can help you get started in Software Engineering!</p>
+                    <div class="about-title">About</div>
+                    <div class="about-text">Learn the ways that SWECC can help you get started in Software Engineering!</div>
                 </div>
                 <div class="goal-message-section">
-                    <h1 class="goal-title">Our Goal</h1>
-                    <p class="goal-text">
-                        SWECC was formed to provide <strong>support, advice,</strong>
+                    <div class="goal-title">Our Goal</div>
+                    <div class="goal-text">
+                        SWECC was formed to provide <strong>support, advice, </strong>
                         and <strong>opportunities</strong> for people ready
                         to start a career in Software engineering but who don't know where
                         to begin. We bring in panels of people currently in the field
@@ -22,7 +22,7 @@ const About = () => {
                         work to give people a head start in their software engineering career!
                         (This is just a quick sample paragraph pls don't judge the writing)(Add a bit
                         more info here blah blah blah)
-                    </p>
+                    </div>
                 </div>
             </div>
         </div>
