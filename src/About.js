2b13efb100de593aeff220738a81d6cc400eb24a
JaydenBalajadia <121709526+JaydenBalajadia@users.noreply.github.com>
2023-04-15 10:15:00 +0000

    Implemented sections of About page

--- src/About.js
+++ src/About.js
@@ -2,16 +2,30 @@ import React from 'react';
 import './About.css';
 const About = () => {
     return (
-        <about>
+        <div class="about">
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Assistant&display=swap');
+            </style>
             <div class="about-section">
                 <div class="about-message-section">
-                    <p1>This is the about message</p1>
+                    <h1 class="about-title">About</h1>
+                    <p class="about-text">Learn the ways that SWECC can help you get started in Software Engineering!</p>
                 </div>
                 <div class="goal-message-section">
-                    <p1>This is the goal message</p1>
+                    <h1 class="goal-title">Our Goal</h1>
+                    <p class="goal-text">
+                        SWECC was formed to provide <strong>support, advice,</strong>
+                        and <strong>opportunities</strong> for people ready
+                        to start a career in Software engineering but who don't know where
+                        to begin. We bring in panels of people currently in the field
+                        to answer questions, help prepare for interviews,
+                        work to give people a head start in their software engineering career!
+                        (This is just a quick sample paragraph pls don't judge the writing)(Add a bit
+                        more info here blah blah blah)
+                    </p>
                 </div>
             </div>
-        </about>
+        </div>
     );
 }
 
