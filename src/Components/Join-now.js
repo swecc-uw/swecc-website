46cccec3ac7d9a1f781cb970c731b6bfad8d86a0
Leah Jia <yuwenj3@uw.edu>
2023-09-02 20:25:09 -0700

    fix box

--- src/Components/Join-now.js
+++ src/Components/Join-now.js
@@ -1,161 +1,152 @@
 import "../CSS/App.css";
 import "../CSS/Join-now.css";
 import React, { useState } from "react";
-import completionBar0 from "../Data/img/join-now-img0.png";
-import completionBar1 from "../Data/img/join-now-img1.png";
-import completionBar2 from "../Data/img/join-now-img2.png";
-import completionBar3 from "../Data/img/join-now-img3.png";
 import career from "../Data/img/career.png";
 import community from "../Data/img/community.png";
 import networking from "../Data/img/networking.png";
 
-const messages = [
+const actions = [
+  <div className="action-item">
     <a className="link-boxes" href="https://discord.com/invite/Pbk4sCEWDY" target="_blank" rel="noopener noreferrer">
-        Join the discord
-    </a>,
-    <a
-        className="link-boxes"
-        href="http://mailman11.u.washington.edu/mailman/listinfo/sweccmailinglist"
+      Join the discord
+    </a>
+  </div>,
+  <div className="action-item">
+    <a className="link-boxes" href="http://mailman11.u.washington.edu/mailman/listinfo/sweccmailinglist" target="_blank" rel="noopener noreferrer">
+      Subscribe to the mailing list
+    </a>
+  </div>,
+  <div className="action-item">
+    <p>
+      <a
+        className="separate-insta-linkedin link-boxes"
+        href="https://instagram.com/swecc.uw?igshid=YmMyMTA2M2Y="
         target="_blank"
         rel="noopener noreferrer"
-    >
-        Join the Mailing List
-    </a>,
-    <p>
-        <a
-            className="separate-insta-linkedin link-boxes"
-            href="https://instagram.com/swecc.uw?igshid=YmMyMTA2M2Y="
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            Instagram
-        </a>
-        <a
-            className="separate-insta-linkedin link-boxes"
-            href="https://www.linkedin.com/company/software-engineering-career-club-at-uw/"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            LinkedIn
-        </a>
-    </p>,
-    <p className="thanks-box thanks-message">
+      >
+        Instagram
+      </a>
+      <a
+        className="separate-insta-linkedin link-boxes"
+        href="https://www.linkedin.com/company/software-engineering-career-club-at-uw/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+    </p>
+  </div>,
+  <div className="action-item">
+    <div className="thanks-box">
+      <p className="thanks-message">
         <strong>Thanks for Joining! </strong>
         <br />
         We're looking forward to seeing
         <br />
         you at our next meeting :)
-    </p>,
-];
-
-const images = [
-    <img src={completionBar0} className="completion-bar" alt="Unfilled completion bar" />,
-    <img src={completionBar1} className="completion-bar" alt="Partly filled completion bar" />,
-    <img src={completionBar2} className="completion-bar" alt="Mostly filled completion bar" />,
-    <img src={completionBar3} className="completion-bar" alt="Filled completion bar" />,
+      </p>
+    </div>
+  </div>,
 ];
 
 export default function JoinNow() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    function increment() {
-        if (count < 3) {
-            setCount(count + 1);
-        }
+  function increment() {
+    if (count < 3) {
+      setCount(count + 1);
     }
+  }
 
-    function decrement() {
-        if (count > 0) {
-            setCount(count - 1);
-        }
+  function decrement() {
+    if (count > 0) {
+      setCount(count - 1);
     }
+  }
 
-    return (
-        <div className="entire">
-            <div className="background-image">
-                <div className="js-white-box">
-                    <div className="content-flex">
-                        <p className="join-message">
-                            <strong>Join SWECC </strong>
-                        </p>
-                        <p className="center-images"> {images[count]} </p>
-                        <p>{messages[count]}</p>
-                    </div>
-                    <div className="button-flex">
-                        {count > 0 && (
-                            <button className="click-button-styling" onClick={decrement}>
-                                Back
-                            </button>
-                        )}
-                        {count < 3 && (
-                            <button className="click-button-styling" onClick={increment}>
-                                Next
-                            </button>
-                        )}
-                    </div>
-                </div>
+  return (
+    <div className="entire">
+      <div className="background-image">
+        <div className="js-white-box">
+          <div className="content-flex">
+            <p className="join-message">
+              <strong>Join SWECC </strong>
+            </p>
+            <div className="stepper-wrapper">
+              <div className={`stepper-item ${count > 0 ? "completed" : count === 0 ? "active" : ""}`}>
+                <div className="step-counter">1</div>
+                <div className="step-name">Join our discord</div>
+              </div>
+              <div className={`stepper-item ${count > 1 ? "completed" : count === 1 ? "active" : ""}`}>
+                <div className="step-counter">2</div>
+                <div className="step-name">Join our mailing list</div>
+              </div>
+              <div className={`stepper-item ${count > 2 ? "completed" : count === 2 ? "active" : ""}`}>
+                <div className="step-counter">3</div>
+                <div className="step-name">Attend an event</div>
+              </div>
             </div>
-            <div className="join">
-                <div className="join-section">
-                    <div className="info-message-section">
-                        <div className="info-table">
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td align="center">
-                                            <img
-                                                className="example-images"
-                                                src={networking}
-                                                alt="Networking"
-                                                width="60%"
-                                            />
-                                        </td>
-                                        <td align="center">
-                                            <img
-                                                className="example-images"
-                                                src={community}
-                                                alt="Community"
-                                                width="60%"
-                                            />
-                                        </td>
-                                        <td align="center">
-                                            <img className="example-images" src={career} alt="Career" width="60%" />
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <div className="info-title">networking</div>
-                                        </td>
-                                        <td>
-                                            <div className="info-title">community</div>
-                                        </td>
-                                        <td>
-                                            <div className="info-title">career</div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <div className="info-text">
-                                                make connections in a network of future and current software engineers
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div className="info-text">
-                                                Join a vibrant community of future software engineers
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div className="info-text">
-                                                Gain access to career talks, and more opportunities
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <p>{actions[count]}</p>
+          </div>
+          <div className="button-container">
+            {count > 0 && (
+              <button className="click-button back-button" onClick={decrement}>
+                Back
+              </button>
+            )}
+            {count < 3 && (
+              <button className="click-button next-button" onClick={increment}>
+                Next
+              </button>
+            )}
+          </div>
         </div>
-    );
+      </div>
+      <div className="join">
+        <div className="join-section">
+          <div className="info-message-section">
+            <div className="info-table">
+              <table>
+                <tbody>
+                  <tr>
+                    <td align="center">
+                      <img className="example-images" src={networking} alt="Networking" width="60%" />
+                    </td>
+                    <td align="center">
+                      <img className="example-images" src={community} alt="Community" width="60%" />
+                    </td>
+                    <td align="center">
+                      <img className="example-images" src={career} alt="Career" width="60%" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div className="info-title">networking</div>
+                    </td>
+                    <td>
+                      <div className="info-title">community</div>
+                    </td>
+                    <td>
+                      <div className="info-title">career</div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div className="info-text">make connections in a network of future and current software engineers</div>
+                    </td>
+                    <td>
+                      <div className="info-text">Join a vibrant community of future software engineers</div>
+                    </td>
+                    <td>
+                      <div className="info-text">Gain access to career talks, and more opportunities</div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
