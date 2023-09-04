2f624c0cc40c40a118697e2a95c0954fa8181fb7
Leah Jia <yuwenj3@uw.edu>
2023-09-03 21:57:36 -0700

    officers info

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../CSS/About.css";
-import Footer from "./Footer.js";
 import teamMembers from "../Data/teamMembers.json";
+import ProfileCard from "./profileCard";
 
 const About = () => {
   const Card = ({ member }) => {
@@ -21,35 +21,21 @@ const About = () => {
   };
 
   return (
-    <>
-      <div className="about">
-        <div className="about-section">
-          <div className="about-message-section">
-            <div className="about-title">About</div>
-            <div className="about-text">Learn the ways that SWECC can help you get started in Software Engineering!</div>
-          </div>
-          <div className="goal-message-section">
-            <div className="goal-title">Our Goal</div>
-            <div className="goal-text">
-              SWECC was formed to provide <strong>support, advice, </strong>
-              and <strong>opportunities</strong> for people ready to start a career in Software engineering but who don't know where to begin. We
-              bring in panels of people currently in the field to answer questions, help prepare for interviews, work to give people a head start in
-              their software engineering career!
-            </div>
-          </div>
-          <div className="team-members-section">
-            <h1 className="team-members-title">Board Members</h1>
-            <p className="team-members-subtitle">2023-2024</p>
-            <div className="picture-container">
-              {teamMembers.map((member) => (
-                <Card key={member.name} member={member} />
-              ))}
-            </div>
+    <div>
+      <div className="about-section">
+        <div className="about-message-section">
+          <div className="about-title">About Us</div>
+          <div className="about-text">Learn the ways that SWECC can help you get started in Software Engineering!</div>
+        </div>
+        <div className="team-members-section">
+          <h1 className="team-members-title">Board Members</h1>
+          <p className="team-members-subtitle">2023-2024</p>
+          <div>
+            <ProfileCard info={teamMembers} />
           </div>
         </div>
-        <Footer />
       </div>
-    </>
+    </div>
   );
 };
 
