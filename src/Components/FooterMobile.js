c1062aecf02efb489805fc9b86d5e3783f9c1f67
Leah Jia <99011487+leahjia@users.noreply.github.com>
2023-09-12 21:49:45 -0700

    footer hotfix mobile

---
+++ src/Components/FooterMobile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import SWECClogo2 from "../Data/img/SWECClogo2.jpg";
+
+export default function Footer() {
+  return (
+    <footer className='hotfix'>
+      <div>
+        <img className="footer-logo" src={SWECClogo2} alt="SWECC logo" />
+        <h3>SWECC @ UW</h3>
+        <ul className="links" >
+            <li><a href="mailto:swecc@uw.edu" target="_blank" rel="noopener noreferrer"><span className="fa fa-envelope"></span></a></li>
+            <li><a href="https://www.linkedin.com/company/software-engineering-career-club-at-uw/" target="_blank" rel="noopener noreferrer"><span className="fa fa-linkedin"></span></a></li>
+            <li><a href="https://github.com/swecc-uw" target="_blank" rel="noopener noreferrer"><span className="fa fa-github"></span></a></li>
+            <li><a href="https://instagram.com/swecc.uw?igshid=YmMyMTA2M2Y=" target="_blank" rel="noopener noreferrer"><span className="fa fa-instagram"></span></a></li>
+            <li><a href="https://discord.com/invite/Pbk4sCEWDY" target="_blank" rel="noopener noreferrer"><span className="fab fa-discord"></span></a></li>
+        </ul>
+      </div>
+      <div className="copyright">
+        <p>Copyright&copy;UW SWECC 2023 | Collaboration with Dubvelopers</p>
+      </div>
+    </footer>
+  )
+}
