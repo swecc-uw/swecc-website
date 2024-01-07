73fb18a25ab8f88b11f7cda13109b31bf598a9f2
Hawa Ali <hawad416@gmail.com>
2024-01-06 23:43:14 -0800

    adding past officers to the site.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../CSS/About.css";
 import teamMembers from "../Data/teamMembers.json";
+import pastOfficers from "../Data/pastOfficers.json";
 import ProfileCard from "./profileCard";
 
 const About = () => {
@@ -15,6 +16,11 @@ const About = () => {
           <h1 className="team-members-title">Board Members</h1>
           <p className="team-members-subtitle">2023-2024</p>
           <ProfileCard info={teamMembers} />
+        </div>
+        <div>
+          <h1 className="team-members-title">Board Members</h1>
+          <p className="team-members-subtitle">2022-2023</p>
+          <ProfileCard info={pastOfficers} />
         </div>
       </div>
     </div>
