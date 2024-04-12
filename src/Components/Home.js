e95c1f443ba62548727f2d18ef678b14f8255d77
Randolph Jenkins <tang1125@uw.edu>
2024-04-11 18:46:23 -0700

    4/11

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,21 @@
 import "../CSS/Home.css";
+import React, {useEffect, useState} from 'react';
 import SWECCmap from "../Data/img/location.png";
 import UWlogo from "../Data/img/UW_logo.png";
 import { InstagramEmbed } from "react-social-media-embed";
 
+import img1 from "../Data/img/frontPageBackgroundImgs/IMG_9224.JPG";
+import img2 from "../Data/img/frontPageBackgroundImgs/DSC01727.jpg";
+import img3 from "../Data/img/frontPageBackgroundImgs/DSC01727.jpg";
+import img4 from "../Data/img/frontPageBackgroundImgs/DSC01917.jpg";
+import img5 from "../Data/img/frontPageBackgroundImgs/DSC01946.jpg";
+import img6 from "../Data/img/frontPageBackgroundImgs/DSC02159.JPG";
+import img7 from "../Data/img/frontPageBackgroundImgs/DSC02159.JPG";
+
 function HomePage() {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
   const meetingInfo = (
     <div className="meetingsSection">
       <div className="meetingsInfoSection">
@@ -33,16 +45,44 @@ function HomePage() {
     </div>
   );
 
+  const backGroundImgs = [img1, img2, img3, img4, img5, img6, img7];
+
+  const nextImg = () => {
+      setCurrentIndex((prevIndex)=>
+          (prevIndex === backGroundImgs.length - 1? 0 : prevIndex + 1));
+  };
+
+  const prevImg = () => {
+      setCurrentIndex((prevIndex)=>
+            (prevIndex === 0? backGroundImgs.length - 1 : prevIndex - 1));
+  };
+
+    useEffect(() => {
+        const intervalId = setInterval(nextImg, 4000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+
   return (
     <div>
-      <div className="clubTitleSection">
-        <img className="uw-logo" src={UWlogo} alt="UW logo" />
-        <h1 className="clubName">Software Engineering Career Club</h1>
-      </div>
-      <div className="whatWeDoSection">
-        <h2 className="summaryTitle">Who we are</h2>
-        <p className="summary">
-          The Software Engineering Career Club (SWECC) is a student-led organization at the University of Washington in Seattle. Our mission is to support and empower individuals interested in pursuing a successful career in software engineering. We offer a wide range of activities and resources,
+        <div className="clubTitleSection">
+
+            <div className={`carousel-background-${currentIndex}`}>
+                <img className="uw-logo" src={UWlogo} alt="UW logo"/>
+                <h1 className="clubName">Software Engineering Career Club</h1>
+                <button onClick={prevImg}>Previous</button>
+                <button onClick={nextImg}>Next</button>
+            </div>
+
+
+        </div>
+
+        <div className="whatWeDoSection">
+            <h2 className="summaryTitle">Who we are</h2>
+            <p className="summary">
+                The Software Engineering Career Club (SWECC) is a student-led organization at the University of
+                Washington in Seattle. Our mission is to support and empower individuals interested in pursuing a successful career in software engineering. We offer a wide range of activities and resources,
           including networking events, LeetCode challenges, mentorship programs, and interview preparation, to help our members thrive in the field. Join SWECC to jumpstart your software engineering career with confidence and connections!
         </p>
       </div>
