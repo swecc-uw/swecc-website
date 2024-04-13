f4402e74f3d45635763aae21fe1374a71d20ed2a
tang1125@uw.edu <tang1125@uw.edu>
2024-04-13 01:30:44 -0700

    front-page title img background 4/13

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import "../CSS/Home.css";
 import React, {useEffect, useState} from 'react';
 import SWECCmap from "../Data/img/location.png";
 import UWlogo from "../Data/img/UW_logo.png";
-import { InstagramEmbed } from "react-social-media-embed";
+import {InstagramEmbed} from "react-social-media-embed";
 
 import img1 from "../Data/img/backgroundImg/1.jpg"
 import img2 from "../Data/img/backgroundImg/2.jpg"
@@ -32,19 +32,24 @@ function HomePage() {
             setCurrentIndex((prevIndex) => (prevIndex === 0 ? backGroundImgs.length - 1 : prevIndex - 1));
         };
 
-        const divStyle = {
-            backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(${backGroundImgs[currentIndex]})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            padding: '20px',
-            transition: 'opacity 3s ease'
-        };
-
         return (
-            <div className="clubTitleSection" style={divStyle}>
+            <div className="clubTitleSection">
+
+                {backGroundImgs.map((img, index) => (
+                    <img
+                        key={index}
+                        src={img}
+                        alt={`Background ${index}`}
+                        className={`background-img ${index === currentIndex ? '' : 'hide'}`}
+                    />
+                ))}
+
                 <div className="buttons-container">
-                    <button onClick={prevImg} className="prevImgButton">&lt;</button>
-                    <button onClick={nextImg} className="nextImgButton">&gt;</button>
+                    <button onClick={prevImg} className="prevImgButton"><span
+                        className="material-symbols-outlined">navigate_before</span></button>
+
+                    <button onClick={nextImg} className="nextImgButton"><span
+                        className="material-symbols-outlined">navigate_next</span></button>
                 </div>
                 <img className="uw-logo" src={UWlogo} alt="UW logo"/>
                 <h1 className="clubName">Software Engineering Career Club</h1>
@@ -57,47 +62,51 @@ function HomePage() {
         <div className="meetingsSection">
             <div className="meetingsInfoSection">
                 <h2 className="sweccMeetings">SWECC Meetings</h2>
-                <p className="meetingInfo">Held in the EAC, these meeting feature topics in a vast number of areas, including professional development, resume building, and mentor circles</p>
-        <h3 className="meetingTime">Meeting Times/Location (Winter 2024)</h3>
-        <p className="meetingDetails">
-          Every week on Wednesday, from 5:30-6:30pm
-          <br />
-          Location: Loew 216
-        </p>
-      </div>
-      <div className="map-and-label">
-        <img className="map" src={SWECCmap} alt="map" />
-        <p className="mapLabel">Location: Loew 216</p>
-      </div>
-    </div>
-  );
+                <p className="meetingInfo">Held in the EAC, these meeting feature topics in a vast number of areas,
+                    including professional development, resume building, and mentor circles</p>
+                <h3 className="meetingTime">Meeting Times/Location (Winter 2024)</h3>
+                <p className="meetingDetails">
+                    Every week on Wednesday, from 5:30-6:30pm
+                    <br/>
+                    Location: Loew 216
+                </p>
+            </div>
+            <div className="map-and-label">
+                <img className="map" src={SWECCmap} alt="map"/>
+                <p className="mapLabel">Location: Loew 216</p>
+            </div>
+        </div>
+    );
 
-  const posts = (
-    <div className="posts">
-      <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/C2-o7roPbfN" />
-      <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/C23PyAWPYdW" />
-      <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/CyRondZPVVS" />
-      <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/Cxum20yOlkQ" />
-      <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/CxrxWcQLpSb" />
-    </div>
-  );
+    const posts = (
+        <div className="posts">
+            <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/C2-o7roPbfN"/>
+            <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/C23PyAWPYdW"/>
+            <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/CyRondZPVVS"/>
+            <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/Cxum20yOlkQ"/>
+            <InstagramEmbed className="instaPost slide-up" url="https://www.instagram.com/p/CxrxWcQLpSb"/>
+        </div>
+    );
 
-  return (
-    <div>
-        <Carousel/>
+    return (
+        <div>
+            <Carousel/>
 
-        <div className="whatWeDoSection">
-            <h2 className="summaryTitle">Who we are</h2>
-            <p className="summary">
-                The Software Engineering Career Club (SWECC) is a student-led organization at the University of
-                Washington in Seattle. Our mission is to support and empower individuals interested in pursuing a successful career in software engineering. We offer a wide range of activities and resources,
-          including networking events, LeetCode challenges, mentorship programs, and interview preparation, to help our members thrive in the field. Join SWECC to jumpstart your software engineering career with confidence and connections!
-        </p>
-      </div>
-      {meetingInfo}
-      {posts}
-    </div>
-  );
+            <div className="whatWeDoSection">
+                <h2 className="summaryTitle">Who we are</h2>
+                <p className="summary">
+                    The Software Engineering Career Club (SWECC) is a student-led organization at the University of
+                    Washington in Seattle. Our mission is to support and empower individuals interested in pursuing a
+                    successful career in software engineering. We offer a wide range of activities and resources,
+                    including networking events, LeetCode challenges, mentorship programs, and interview preparation, to
+                    help our members thrive in the field. Join SWECC to jumpstart your software engineering career with
+                    confidence and connections!
+                </p>
+            </div>
+            {meetingInfo}
+            {posts}
+        </div>
+    );
 }
 
 export default HomePage;
